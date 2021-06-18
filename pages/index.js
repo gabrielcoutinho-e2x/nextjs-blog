c949551c9054db54813c75f95197cf054f3ac006
Gabriel Fonseca <gabrielfonseca.coutinho@outlook.com>
2021-06-18 09:46:31 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,77 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import { useEffect } from "react";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  useEffect(() => {
+    window.addEventListener(
+      "message",
+      function (event) {
+        console.log("Received post message", event);
+      },
+      false
+    );
+
+    return () => {
+      window.removeEventListener(
+        "message",
+        function (event) {
+          console.log("Received post message", event);
+        },
+        false
+      );
+    };
+  });
+
+  const sendMessage = () => {
+    console.log("Send post message");
+    window.postMessage("Post message from web", "*");
+  };
+
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <Button onClick={sendMessage}>Show Toast</Button>
+    </Layout>
+  );
+}
